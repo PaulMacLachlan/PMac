9dc99c651f3cd2f639cf77603ea90aacf9ee2e04
PaulMacLachlan <paulmacother@gmail.com>
2016-12-10 14:18:02 -0800

    updating repo for todays work

---
+++ Algorithms/12_8_2016/reverse_array.js
@@ -0,0 +1,9 @@
+reverse array
+
+arr = [1, 2, 3, 4, 5]
+
+store the last index, overwrite it with the first index, repeat as many times as the number of indicies.
+
+
+
+for (var idx = 0; idx < arr.length - 1; idx += 1;)
