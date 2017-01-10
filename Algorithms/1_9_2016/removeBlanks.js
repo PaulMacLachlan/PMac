61cacd1b1240b6374af60e53248bf06191becbef
PaulMacLachlan <paulmacother@gmail.com>
2017-01-09 19:44:00 -0800

    new work to update remote

---
+++ Algorithms/1_9_2016/removeBlanks.js
@@ -0,0 +1,13 @@
+function removeBlanks(str){
+    var l = string.length - 1
+    for (var i = 0; i < l; i++) {
+        if (str[i] === '/ /') {
+            return;
+        }
+        else if (str[i] != '/ /') {
+            var newStr = str[i];
+        }
+        console.log(newStr)l;
+        }
+    }
+}
