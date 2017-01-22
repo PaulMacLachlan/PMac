401705976b0ffbdd2e289ca164e08a05ddd8f8ec
PaulMacLachlan <paulmacother@gmail.com>
2017-01-21 19:55:50 -0800

    still resolving issues with GIT repos

--- Python/landing_page/landing_page.py
+++ Python/landing_page/landing_page.py
@@ -3,14 +3,14 @@ app = Flask(__name__)
 
 @app.route('/') #our index page route
 def landingPage():
-    return render_template('index.html')
+    return render_template('index.html', style = ../static/style.css)
 
 @app.route('/ninjas.html')
 def ninjas():
-    return render_template('/ninjas.html')
+    return render_template('/ninjas.html', style = ../static/style.css)
 
 @app.route('/dojos.html')
 def dojosNew():
-    return render_template('dojos.html')
+    return render_template('dojos.html', style = ../static/style.css)
 
 app.run(debug=True)
