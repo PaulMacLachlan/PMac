d862d57044deddf0516b0256a573bfaff8c45d68
PaulMacLachlan <paulmacother@gmail.com>
2017-01-26 07:46:50 -0800

    updating last nights work

--- Python/ninja_gold/ninja_gold_FIX.py
+++ Python/ninja_gold/ninja_gold_FIX.py
@@ -1,22 +1,24 @@
-from flask import Flask, render_template, session, flash, redirect, request
+from flask import Flask, render_template, session, request, flash, redirect
 import random
 
 app = Flask(__name__)
+#this needs to be here to make session work
 app.secret_key = 'YouWereMissingThis'
-
 
 @app.route('/')
 def ninjaGold():
     return render_template('index.html')
 
 @app.route('/process_money', methods=['POST'])
-def processFarm(): #heres where the problem is
-    result = request.form
-    if result != 0:
+def processFarm(): #heres where the problem is, need the maths!
+#I changed your "name" values in your html, they should all be the same, and the "value" should be different, i.e. farm, cave, house, casino, the if statement for farm below represents how to call it. I'm still getting that key error and I don't know why.
+    if request.form['building'] == 'farm':
         print "You chose Farm!"
         procFarm = random.randint(10,20)
         print procFarm
-        return redirect('/', gold = 'current_gold')
+        session['current_gold'] += procFarm
+        print session['current_gold']
+        return redirect('/', session['gold'])
 
     elif session['cave'] != 0:
         print "You chose Cave!"
