3f8ab30dd25ba27c7475f7493935a21cb2b1532f
PaulMacLachlan <paulmacother@gmail.com>
2017-01-28 22:07:36 -0800

    updating changes to dojo survery with new error messaging

--- Python/dojo_survey_validations/counter.py
+++ Python/dojo_survey_validations/counter.py
@@ -9,18 +9,27 @@ def home():
 
 @app.route('/result', methods=['POST'])
 def resultPage():
-    print 'I\'m in the result page function'
+    error_messages = {} #ideally, there is a way to eval ALL possible error situations, store their given error messages, and server each of those error(s) to the page where appropriate. Here, we are hitting the first error we encounter, and not considering ALL CASES when outputting errors.
     print request.form
     if len(request.form['name']) <= 0:
-        flash("* Name is required")#individual error messages for Name and Comments not being present.
-        print flash # trying to see flash messages to see if multiple could be printed or if only one message at a time is supported.
+        error_messages['name_error'] = "* Name is required"
+        flash(error_messages['name_error'])#individual error messages for Name and Comments not being present.
         return redirect ('/')
 
-    elif len(request.form['comment']) <= 0:
-        flash("* Comments are required")#individual error messages for Name and Comments not being present.
-        print flash # trying to see flash messages to see if multiple could be printed or if only one message at a time is supported.
+    if len(request.form['comment']) <= 0:
+        error_messages['comment_required'] = "* Comments are required"
+        flash(error_messages['comment_required'])#individual error messages for Name and Comments not being present.
+        return redirect ('/')
+
+    if len(request.form['comment']) > 120:
+        error_messages['comment_long'] = "* Comments can be no more that 120 characters"
+        flash(error_messages['comment_long'])#individual error messages for Name and Comments not being present.
         return redirect ('/')
     else:
         return render_template('result.html', data = request.form)
 
+    for keys in error_messages:
+        errors_list = []
+        errors_list.append(keys)
+        print errors_list
 app.run(debug=True)
