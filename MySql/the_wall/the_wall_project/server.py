c3ef2111a82c5c7d0ef695ecd3f61f230fd03dec
PaulMacLachlan <paulmacother@gmail.com>
2017-02-01 23:43:07 -0800

    all necessary routes added, building out functionality next

--- MySql/the_wall/the_wall_project/server.py
+++ MySql/the_wall/the_wall_project/server.py
@@ -11,6 +11,8 @@ print mysql.query_db("SELECT * FROM users")
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
+    # the initial root index registration view.
+
     #error if no input provided
     #take in user signin data
     #route it to db (POST)
@@ -18,15 +20,39 @@ def index():
     # return the username for header
     return render_template('registration.html')
 
+@app.route('/register')
+def register():
+    #handles the new user creation functions
+
+    return render_template('registration.html')
+
+@app.route('/login')
+def login():
+    # logging in a user
+    return render_template('wall.html')
+
 @app.route('/logout', methods=['POST'])
 def logOut():
+    #logs us out, returns us to registration page.
     print "we are redirecting to '/'!"
     return redirect('/')
 
 @app.route('/wall', methods=['POST']) #needs 'POST' method???
 def wall(): #takes an ID???
+    #displays the main comments page with comments and messages.
+
     #for displaying the comments page only?
     #has ability to reroute when new comments/messages update?
+    return render_template('wall.html')
+
+@app.route('/message')
+def message():
+    #for committing messages
+    return render_template('wall.html')
+
+@app.route('/comment')
+def comment():
+    #for committing comments
     return render_template('wall.html')
 
 app.run(debug=True)
