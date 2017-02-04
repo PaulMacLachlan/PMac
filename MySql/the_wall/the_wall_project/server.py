5042f68e693d1bcba83e9df45b5f705c74e95bcb
PaulMacLachlan <paulmacother@gmail.com>
2017-02-04 00:15:29 -0800

    2nd attampt at wall work

--- MySql/the_wall/the_wall_project/server.py
+++ MySql/the_wall/the_wall_project/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, session, render_template, redirect, flash
+from flask import Flask, session, render_template, redirect, flash, request
 from flask_bcrypt import Bcrypt
 import re
 
@@ -10,62 +10,51 @@ bcrypt = Bcrypt(app)
 #regex for email validation:
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9\.\+_-]+@[a-zA-Z0-9\._-]+\.[a-zA-Z]*$')
 
-app.secret_key = "SUPERSECRET"
+app.secret_key = "SECRET"
 
-from mysqlconnection import MySQLConnector
-# connect and store the connection in "mysql" note that you pass the database name to the function
-mysql = MySQLConnector(app, 'wall_database')
-# an example of running a query
+from mysqlconnection import MySQLConnector # connect and store the connection in "mysql" note that you pass the database name to the function
+mysql = MySQLConnector(app, 'wall_database') # an example of running a query
 print mysql.query_db("SELECT * FROM users")
+
+@app.route('/register', methods=['POST'])
+def register():
+    #handles the new user creation functions
+    form = request.form
+    errors = False
+
+    if form[confirm_password] != form[password]:
+        errors = True
+        flash('passwords do not match!', 'errors')
+        return redirect('/')
+
+    if errors == True:
+        flash('error')
+        return redirect('/')
+
+    # elif
+    #     request.form[first_name]:
+    #     first_name = request.form['first_name']
+    #     last_name = request.form['last_name']
+    #     email = request.form['email']
+    #     pw_hash = bcrypt.generate_password_hash(password)
+
+    elif errors != True:
+
+        insert_query = "INSERT INTO users (first_name, last_name, email, password, created_at, updated_at) VALUES (:first_name, :last_name, :email, :password, NOW(), NOW())"
+        query_data = { 'first_name': first_name, 'last_name': last_name, 'email': email, 'password': password }
+        mysql.query_db(insert_query, query_data)
+        return render_template('registration.html')
+
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     # the initial root index registration view.
     print session
     print "thats session!"
-    #error if no input provided
-    #take in user signin data
-    #route it to db (POST)
-    # query for db
-    # return the username for header
-
     try:
         session['is_logged_in'] #validate if registration/login is successful, and send them to the wall if need be
         return redirect('/wall')
     except: pass
-    return render_template('registration.html')
-
-@app.route('/register', methods=['POST'])
-def register():
-    #handles the new user creation functions
-    form = request.form #local copy to expedite processing
-    print "I am form:",form
-    #error handling:
-    errors = False #sets up a state where "there are no errors" and we can call true when one is detected.
-
-    # First Name - letters only, at least 2 characters and that it was submitted
-    if not request.form[first_name]:
-        errors = True
-        flash('error', 'errors')
-        first_name = request.form['first_name']
-
-    # Last Name - letters only, at least 2 characters and that it was submitted
-    last_name = request.form['last_name']
-
-    # Email - Valid Email format, and that it was submitted
-    email = request.form['email']
-
-    # Password - at least 8 characters, and that it was submitted
-    password = md5.new(request.form['password']).hexdigest();
-
-    # Password Confirmation - matches password
-    if form[confirm_password] != form[password]:
-        errors = True
-        flash('passwords do not match!', 'errors')
-
-    insert_query = "INSERT INTO users (first_name, last_name, email, password, created_at, updated_at) VALUES (:first_name, :last_name, :email, :password, NOW(), NOW())"
-    query_data = { 'first_name': first_name, 'last_name': last_name, 'email': email, 'password': password }
-    mysql.query_db(insert_query, query_data)
     return render_template('registration.html')
 
 @app.route('/login')
---
+++ MySql/the_wall/the_wall_project/thewall2/server.py
@@ -0,0 +1,41 @@
+from flask import Flask, session, redirect, render_template, flash, request
+from mysqlconnection import MySQLConnector
+from flask_bcrypt import Bcrypt
+app = Flask(__name__)
+bcrypt = Bcrypt(app)
+mysql = MySQLConnector(app, 'wall_2_database')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/register', methods=['POST'])
+def register():
+    # register user, insecure v1.0
+    form = request.form
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    email = request.form['email']
+    password = request.form['password']
+    confirm = request.form['confirm']
+    insert_query = "INSERT INTO users (first_name, last_name, email, password, confirm, created_at, updated_at) VALUES (:first_name, :last_name, :email, :password, :confirm, NOW(), NOW())"
+    query_data = { 'first_name': first_name, 'last_name': last_name, 'email': email, 'password': password, 'confirm': confirm }
+    mysql.query_db(insert_query, query_data)
+    return render_template('wall.html')
+
+# route for only signing in
+@app.route('/login', methods=['POST'])
+def login():
+    return redirect('wall.html')
+
+# route for signing out
+@app.route('/logout', methods=['POST'])
+def logout():
+    session.pop
+    return redirect('/')
+
+@app.route('/wall')
+def wall():
+    return render_template('wall.html')
+
+app.run(debug=True)
