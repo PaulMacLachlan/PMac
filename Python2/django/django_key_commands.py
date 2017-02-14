be1264a4295f3fa5fc5961443409746d21d89b51
PaulMacLachlan <paulmacother@gmail.com>
2017-02-13 22:27:24 -0800

    updating Django work

---
+++ Python2/django/django_key_commands.py
@@ -0,0 +1,10 @@
+# Django Key Commands:
+
+
+# startup virtual envs: source djangoEnv/bin/activate
+# Create a new project: django-admin startproject 'projectname'
+# create a new app (in same dir as manage.py): python manage.py startapp first_app
+
+# start django server: python manage.py runserver
+# # add your app to the settings.py file in main project folder: INSTALLED_APPS = [
+#     'apps.first_app', (for example)
