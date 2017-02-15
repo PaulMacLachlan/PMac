9be3eaa539eab005d30d5d4f0226386e79f2f9ae
PaulMacLachlan <paulmacother@gmail.com>
2017-02-14 23:14:08 -0800

    now functional time display assignment

--- Python2/django/time_display_assignment/apps/timedisplay/views.py
+++ Python2/django/time_display_assignment/apps/timedisplay/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, HttpResponse
+from datetime import datetime
 
 # Create your views here.
 def index(request):
+    timestamp = datetime.now()
     context = {
-    "timetest": [1,2,3,4,5],
-    "t2": "string"
+        'date': timestamp.strftime("%b %d, %Y"),
+        'time': timestamp.strftime("%I: %M %p")
     }
-    return render(request, 'timedisplay/index.html')
+    return render(request, 'timedisplay/index.html', context)
 
 def show(request):
     print(request.method)
