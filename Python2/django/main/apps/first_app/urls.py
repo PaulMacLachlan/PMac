1174299e5f408d1e87557f7389591d377a1c3146
PaulMacLachlan <paulmacother@gmail.com>
2017-02-14 20:43:26 -0800

    last nights work, prep for tonights

--- Python2/django/main/apps/first_app/urls.py
+++ Python2/django/main/apps/first_app/urls.py
@@ -2,12 +2,7 @@ from django.conf.urls import url
 from . import views
 # from django.contrib import admin
 
-def index(request):
-    print ("8"*100)
-    print "Whatever route that was hit by an HTTP request (by the way) decided to invoke me!"
-    print "By the way, here's the request object that Django automatically passes us:", request
-    print "By the by, we still aren't delivering anything to the browser, so you should see 'ValueError at /'"
-    print ("8"*100)
+
 
 urlpatterns = [
     url(r'^$', views.index)
--- Python2/django/main/apps/first_app/views.py
+++ Python2/django/main/apps/first_app/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+# CONTROLLER!!!
 
 # Create your views here.
 
 def index(request):
     response = "Hello, I am your first request!"
+    print "Hello, I am your first request!","<--- this should be in your browser!"
+    # response = "And I am another!"
+    # print "And I am another!"
     return HttpResponse(response)
+
+
+def index(request):
+    print ("*"*100)
+    return render(request, "first_app/index.html", )
