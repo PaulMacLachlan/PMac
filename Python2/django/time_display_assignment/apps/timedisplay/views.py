62bf2436dc7a8748fb87b7db604d85c776de72ea
PaulMacLachlan <paulmacother@gmail.com>
2017-02-14 22:38:14 -0800

    semi functional time display assignment

--- Python2/django/time_display_assignment/apps/timedisplay/views.py
+++ Python2/django/time_display_assignment/apps/timedisplay/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def index(request):
+    context = {
+    "somekey":"somevalue"
+    }
+    return render(request, 'timedisplay/index.html')
+
+def show(request):
+    print(request.method)
+    # context = {
+    # "somekey":"somevalue"
+    # }
+    return render(request, 'timedisplay/show.html')
