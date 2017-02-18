5ae84d6bdd76996e800ece5f77bc5e89c4d0a6d2
PaulMacLachlan <paulmacother@gmail.com>
2017-02-18 13:55:12 -0800

    Updates.

--- Python2/django/survey_form_assignment/apps/survey_form/urls.py
+++ Python2/django/survey_form_assignment/apps/survey_form/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^process$', views.process),
     url(r'^', views.index),
     # url(r'^result$', include('apps.survey.urls'))
 ]
--- Python2/django/survey_form_assignment/apps/survey_form/views.py
+++ Python2/django/survey_form_assignment/apps/survey_form/views.py
@@ -1,23 +1,25 @@
 from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
 
 # Create your views here.
-
 def index(request):
+    print request.session
     # context = {
-    # 'first_name': request.form('first_name'),
-    # 'dojo_location': request.form('dojo_location'),
-    # 'fav_lang': request.form('fav_lang'),
-    # 'comments': request.form('comments')
+    # 'first_name': request.POST('first_name'),
+    # 'dojo_location': request.POST('dojo_location'),
+    # 'fav_lang': request.POST('fav_lang'),
+    # 'comments': request.POST('comments')
     # }
     return render(request, 'survey_form/index.html') #, context)
 
-def result(request):
-    print (request.method)
-    return render(request)
-    return render(request, 'survey_form/result.html')
+# def result(request):
+#     response = "Hello World!"
+#     print (request.method)
+#     return render(request, 'survey_form/result.html')
 
 def process(request):
     if request.method == 'POST':
+        print ("$" * 10)
         print request.POST
-        print request.method
+        # print request.method
         return redirect('/result')
