47b388267066aa3f9cf4a07d0190a6f9b43bae93
PaulMacLachlan <paulmacother@gmail.com>
2017-02-18 22:35:16 -0800

    Tonights work

--- Python2/django/main2/apps/second_app/views.py
+++ Python2/django/main2/apps/second_app/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
+from .models import People
 
 # Create your views here.
 
 def index(request):
     context = {
-        "email": "blob@gmail.com",
-        "name": "mike"
+        "email": "fakeassemail@gmail.com",
+        "name": "paul"
     }
     print context
     return render(request, "second_app/index.html", context)
@@ -17,3 +18,10 @@ def show(request, id):
     }
     print context
     return render(request, "second_app/show.html", context)
+
+
+#####
+def index(request):
+    People.objects.create(first_name="Paul",last_name="MacLachlan")
+    people = Peopel.objects.all()
+    print Peoplereturn render(request,"second_app/index.html")
