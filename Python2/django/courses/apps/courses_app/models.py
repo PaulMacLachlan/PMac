84e22d7ca59267fa347ab304b8d16e591006c6b7
PaulMacLachlan <paulmacother@gmail.com>
2017-02-20 22:56:56 -0800

    updates

--- Python2/django/courses/apps/courses_app/models.py
+++ Python2/django/courses/apps/courses_app/models.py
@@ -3,3 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=40)
+    description = models.TextField(max_length=1000)
+    created_at = models.DateTimeField(auto_now_add=True)
--- Python2/django/courses/apps/courses_app/views.py
+++ Python2/django/courses/apps/courses_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+from . models import Course
+
 # Create your views here.
+def index(request):
+    print "This is the index Route!"
+    return render(request, "index.html")
